ddf8f69d6cdc74800143213c334fe62f19b773e8
Valto Silva <valtosilva7@gmail.com>
2023-03-17 21:57:10 -0300

    CLIENT => Created Shopping Cart Page Design Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 
 function App() {
-  return <Login />;
+  return <Cart />;
 }
 
 export default App;
